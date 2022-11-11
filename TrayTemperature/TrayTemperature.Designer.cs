913db0c4303f6886bf6e1eb9f2c506b7fa451887
TweenzY-Y <tweenzy.mail@gmail.com>
2022-11-11 14:33:33 +0100

    Add project files.

---
+++ TrayTemperature/TrayTemperature.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace TrayTemperature
+{
+    partial class TrayTemperature
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrayTemperature));
+            this.tray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.trayMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.runOnStartupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trayMenuStripChangeDisplayColorItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trayMenuStripRgbValue = new System.Windows.Forms.ToolStripTextBox();
+            this.trayMenuStripResetLocationItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trayMenuStripExitItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trayMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tray
+            // 
+            this.tray.ContextMenuStrip = this.trayMenuStrip;
+            this.tray.Icon = ((System.Drawing.Icon)(resources.GetObject("tray.Icon")));
+            this.tray.Text = "Tray Temperature v2.0";
+            this.tray.Visible = true;
+            // 
+            // trayMenuStrip
+            // 
+            this.trayMenuStrip.BackColor = System.Drawing.SystemColors.Window;
+            this.trayMenuStrip.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trayMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runOnStartupToolStripMenuItem,
+            this.trayMenuStripChangeDisplayColorItem,
+            this.trayMenuStripResetLocationItem,
+            this.trayMenuStripExitItem});
+            this.trayMenuStrip.Name = "trayMenuStrip";
+            this.trayMenuStrip.Size = new System.Drawing.Size(223, 112);
+            this.trayMenuStrip.Text = "Tray Temperature 2.0";
+            // 
+            // runOnStartupToolStripMenuItem
+            // 
+            this.runOnStartupToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.runOnStartupToolStripMenuItem.Checked = true;
+            this.runOnStartupToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.runOnStartupToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.runOnStartupToolStripMenuItem.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.runOnStartupToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.runOnStartupToolStripMenuItem.Name = "runOnStartupToolStripMenuItem";
+            this.runOnStartupToolStripMenuItem.Padding = new System.Windows.Forms.Padding(0);
+            this.runOnStartupToolStripMenuItem.Size = new System.Drawing.Size(222, 20);
+            this.runOnStartupToolStripMenuItem.Text = "Run on startup";
+            this.runOnStartupToolStripMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.runOnStartupToolStripMenuItem.CheckedChanged += new System.EventHandler(this.runOnStartupToolStripMenuItem_CheckedChanged);
+            // 
+            // trayMenuStripChangeDisplayColorItem
+            // 
+            this.trayMenuStripChangeDisplayColorItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.trayMenuStripChangeDisplayColorItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.trayMenuStripRgbValue});
+            this.trayMenuStripChangeDisplayColorItem.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.trayMenuStripChangeDisplayColorItem.Name = "trayMenuStripChangeDisplayColorItem";
+            this.trayMenuStripChangeDisplayColorItem.Size = new System.Drawing.Size(222, 22);
+            this.trayMenuStripChangeDisplayColorItem.Text = "Change display color (RGB)";
+            this.trayMenuStripChangeDisplayColorItem.DropDownClosed += new System.EventHandler(this.trayMenuStripChangeDisplayColorItem_DropDownClosed);
+            // 
+            // trayMenuStripRgbValue
+            // 
+            this.trayMenuStripRgbValue.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trayMenuStripRgbValue.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.trayMenuStripRgbValue.Name = "trayMenuStripRgbValue";
+            this.trayMenuStripRgbValue.Size = new System.Drawing.Size(100, 23);
+            // 
+            // trayMenuStripResetLocationItem
+            // 
+            this.trayMenuStripResetLocationItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.trayMenuStripResetLocationItem.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.trayMenuStripResetLocationItem.Name = "trayMenuStripResetLocationItem";
+            this.trayMenuStripResetLocationItem.Size = new System.Drawing.Size(222, 22);
+            this.trayMenuStripResetLocationItem.Text = "Reset location";
+            this.trayMenuStripResetLocationItem.Click += new System.EventHandler(this.trayMenuStripResetLocationItem_Click);
+            // 
+            // trayMenuStripExitItem
+            // 
+            this.trayMenuStripExitItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.trayMenuStripExitItem.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.trayMenuStripExitItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.trayMenuStripExitItem.Name = "trayMenuStripExitItem";
+            this.trayMenuStripExitItem.Size = new System.Drawing.Size(222, 22);
+            this.trayMenuStripExitItem.Text = "Exit";
+            this.trayMenuStripExitItem.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.trayMenuStripExitItem.Click += new System.EventHandler(this.trayMenuStripExitItem_Click);
+            // 
+            // TrayTemperature
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TrayTemperature";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tray Temperature v2.0";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.TrayTemperature_Load);
+            this.trayMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon tray;
+        private System.Windows.Forms.ContextMenuStrip trayMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem runOnStartupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trayMenuStripChangeDisplayColorItem;
+        private System.Windows.Forms.ToolStripMenuItem trayMenuStripResetLocationItem;
+        private System.Windows.Forms.ToolStripMenuItem trayMenuStripExitItem;
+        private System.Windows.Forms.ToolStripTextBox trayMenuStripRgbValue;
+    }
+}
+
---
+++ TrayTemperature/TrayTemperature.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using TrayTemperature.Properties;
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
+
+namespace TrayTemperature
+{
+    public partial class TrayTemperature : Form
+    {
+        
+        //Web scraping
+        HtmlWeb htmlReader = new HtmlWeb();
+        HtmlAgilityPack.HtmlDocument htmlDoc;
+
+
+        //Variables
+        string temperature;
+
+        //Registry key for "Run on startup" feature
+        RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+        public TrayTemperature()
+        {
+            InitializeComponent();
+        }
+
+
+        private void TrayTemperature_Load(object sender, EventArgs e)
+        {
+
+            // Loads user's settings
+            LoadUserSettings();
+            SetStartup();
+
+            
+            // Checks if user has an assigned location
+            if (String.IsNullOrEmpty(Settings.Default.userLocation))
+            {
+                GetUserLocation();
+            }
+
+            GetTemperatureFrom(Settings.Default.userLocation);
+            ShowInTray(temperature);
+
+        }
+        
+       // Gets user's location
+        private void GetUserLocation()
+        {
+            const string locationURL = "https://mylocation.org/";
+
+            htmlDoc = htmlReader.Load(locationURL);
+
+            Settings.Default.userLocation = htmlDoc.DocumentNode.SelectNodes("//tr //td")[11].InnerText;
+            Settings.Default.Save();
+        }
+
+        // Gets temperature from users location
+        private void GetTemperatureFrom(string userLocation)
+        {
+            string temperatureURL = $"https://www.msn.com/en-us/weather/forecast/in-{userLocation}?&weadegreetype=C";
+            string testTemperature;
+            byte attempt = 0;
+            
+            htmlDoc = htmlReader.Load(temperatureURL);
+
+            while (attempt <= 1)
+            {
+                try
+                {
+                    testTemperature = htmlDoc.DocumentNode.QuerySelector("a.summaryTemperatureCompact-E1_1.summaryTemperatureHover-E1_1").InnerText.Replace("°C", "");
+                    temperature = testTemperature;
+                    break;
+                }
+                catch (NullReferenceException)
+                {
+                    attempt++;
+                    Task.Delay(1854);
+                }
+            }
+
+        }
+
+        // Display temperature as an icon
+        private void ShowInTray(string temperature)
+        {
+            
+            Brush brush = new SolidBrush(Settings.Default.userColor);
+
+            // Makes 16x16 graphic area to draw an icon
+            Bitmap bitmap = new Bitmap(16, 16);
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            // Positions string in the center of the area
+            Rectangle rect1 = new Rectangle(0, 1, 16, 16);
+            StringFormat stringFormat = new StringFormat();
+            stringFormat.Alignment = StringAlignment.Center;
+            stringFormat.LineAlignment = StringAlignment.Center;
+
+           // Sets font for the icon
+            Font iconFont = new Font("Arial", 8, FontStyle.Regular, GraphicsUnit.Point);
+            
+            // Draws temperature
+            graphics.DrawString(temperature, iconFont , brush, rect1, stringFormat);
+
+            // Displays the icon in tray
+            Icon icon = Icon.FromHandle(bitmap.GetHicon());
+            tray.Icon = icon;
+        }
+
+        // Exits application on click
+        private void trayMenuStripExitItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Resets user's location
+        private void trayMenuStripResetLocationItem_Click(object sender, EventArgs e)
+        {
+            GetUserLocation();
+            GetTemperatureFrom(Settings.Default.userLocation);
+            ShowInTray(temperature);
+        }
+
+       // Changes icon display color
+        private void trayMenuStripChangeDisplayColorItem_DropDownClosed(object sender, EventArgs e)
+        {
+            try
+            {
+                Settings.Default.userColor = ColorTranslator.FromHtml(trayMenuStripRgbValue.Text);
+                ShowInTray(temperature);
+                Settings.Default.Save();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid color format");
+                trayMenuStripRgbValue.Text = $"{Settings.Default.userColor.R},{Settings.Default.userColor.G},{Settings.Default.userColor.B}";
+            }
+        }
+
+        // Loads user's settings
+        private void LoadUserSettings()
+        {
+            runOnStartupToolStripMenuItem.Checked = Settings.Default.runOnStartup;
+            trayMenuStripRgbValue.Text = $"{Settings.Default.userColor.R},{Settings.Default.userColor.G},{Settings.Default.userColor.B}";
+        }
+
+       // Adds or removes program from startup
+        private void SetStartup()
+        {
+            if (runOnStartupToolStripMenuItem.Checked)
+            {
+                key.SetValue("Tray Temperature", Application.ExecutablePath);
+                Settings.Default.runOnStartup = true;
+                Settings.Default.Save();
+            }
+
+            else
+            {
+                key.DeleteValue("Tray Temperature", false);
+                Settings.Default.runOnStartup = false;
+                Settings.Default.Save();
+            }
+        }
+
+        private void runOnStartupToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            SetStartup();
+        }
+    }
+}
